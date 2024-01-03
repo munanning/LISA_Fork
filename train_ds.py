44ff557871636795736e415da47b6e1e87ccdde9
munanning <276585665@qq.com>
2024-01-03 09:26:16 +0800

    revise train_ds

--- train_ds.py
+++ train_ds.py
@@ -73,7 +73,7 @@ def parse_args(args):
         type=int,
     )
     parser.add_argument("--val_batch_size", default=1, type=int)
-    parser.add_argument("--workers", default=8, type=int)
+    parser.add_argument("--workers", default=4, type=int)
     parser.add_argument("--lr", default=0.0003, type=float)
     parser.add_argument("--ce_loss_weight", default=1.0, type=float)
     parser.add_argument("--dice_loss_weight", default=0.5, type=float)
